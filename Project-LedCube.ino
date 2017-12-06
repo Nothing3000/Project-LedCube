d99d20680a7fc034bb1b56d05bfc2ac70e80d2b1
Nothing3000 <34283653+Nothing3000@users.noreply.github.com>
2017-12-06 14:00:05 +0100

    Added .ino.

---
+++ Project-LedCube.ino
@@ -0,0 +1,29 @@
+#include "Shifter.h"
+#include "LedPattern.h"
+
+//Voorbeeld pinnummers
+#define SERPIN 4
+#define SCLKPIN 5
+#define LCLKPIN 6
+#define ROWPIN1 2
+#define ROWPIN2 3
+
+uint8_t rowPins[] = {ROWPIN1, ROWPIN2};
+
+uint8_t pattern[2][8] = {{LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH},
+                         {HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW}};
+                         
+Shifter *shifter = newShifter(SERPIN, SCLKPIN, LCLKPIN, 8);
+LedPattern *leds = newLedPattern(2, 8, rowPins, shifter);
+
+
+void setup() 
+{
+  setLedPattern(leds, &pattern[0][0]);
+}
+
+void loop()
+{
+  updateLeds(leds);
+}
+
