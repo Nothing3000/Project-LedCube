831d5401b8f54ccc00a87ef886f37f18c377f7d0
Nothing3000 <34283653+Nothing3000@users.noreply.github.com>
2017-12-06 13:49:28 +0100

    Fixed the code after a faulty push

--- Project-LedCube.ino
+++ Project-LedCube.ino
@@ -1,29 +1,21 @@
 #include "Shifter.h"
-#include "LedPattern.h"
-
 
 //Voorbeeld pinnummers
 #define SERPIN 4
 #define SCLKPIN 5
 #define LCLKPIN 6
-#define ROWPIN1 2
-#define ROWPIN2 3
 
-
-uint8_t colPins[] = {ROWPIN1, ROWPIN2};
-uint8_t pattern[2][8] = {{LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH},
-                         {HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW}};
+uint8_t pattern[8] = {LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH};
                          
 Shifter *shifter = newShifter(SERPIN, SCLKPIN, LCLKPIN, 8);
-LedPattern *leds = newLedPattern(2, 8, colPins, shifter);
 
 void setup() 
 {
-  setLedPattern(leds, &pattern[0][0]);
+  setShifterPattern(shifter, pattern);
 }
 
 void loop()
 {
-  updateLeds(leds);
+  
 }
 
